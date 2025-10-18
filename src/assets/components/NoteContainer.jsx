35200543c8af91068c0138ff51c56463187e45e7
LoloSac <lorenzosacerdote@gmail.com>
2025-10-18 15:50:31 -0300

    Fixed height and width of the page

--- src/assets/components/NoteContainer.jsx
+++ src/assets/components/NoteContainer.jsx
@@ -2,7 +2,7 @@ import NoteItem from "./NoteItem";
 
 export default function NoteContainer({ notes }) {
     return (
-        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', width: '100vw' }}>
+        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', width: '100%' }}>
             {notes.map((note) => (<NoteItem key={note.id} title={note.title} note={note.content} ></NoteItem>))}
         </div>
     )
